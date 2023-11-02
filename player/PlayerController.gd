925b3b8e1906f8a6ea89a31fa422b62e273ad34a
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-02 13:49:11 +0100

    Export player movement variables

--- player/PlayerController.gd
+++ player/PlayerController.gd
@@ -3,8 +3,8 @@ extends CharacterBody3D
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var SPEED = 5.0
+@export var JUMP_VELOCITY = 4.5
 
 @export var mouse_sensitivity = 0.05
 var mouse_move : Vector2 = Vector2(0, 0)
@@ -25,7 +25,8 @@ func _physics_process(delta):
 	# Mouse mode
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		
+	
+	# Camera rotation
 	camera_rotation(mouse_move, delta)
 	mouse_move.x = 0
 	mouse_move.y = 0
@@ -38,6 +39,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Handle movement
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
