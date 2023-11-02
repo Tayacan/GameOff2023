28357aed5d53c9bf84c52cc57087409de81f800b
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-02 13:12:30 +0100

    Limit velocity change while jumping

--- player/PlayerController.gd
+++ player/PlayerController.gd
@@ -41,8 +41,12 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+	if is_on_floor():
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 2)
+		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 2)
 
 	move_and_slide()
 
