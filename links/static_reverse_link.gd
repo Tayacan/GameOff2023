884b59a35bf2a8e676d9d61e10eb638f2a33161a
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-07 20:30:34 +0100

    Reverse link (finished?)

--- links/static_reverse_link.gd
+++ links/static_reverse_link.gd
@@ -8,33 +8,35 @@ var obj_2_vel : Vector3 = Vector3(0, 0, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
-	var total_mass = obj_1.mass + obj_2.mass
-	var combined_vel_1 = (obj_1_vel * obj_1.mass / total_mass) - (obj_2_vel * obj_2.mass / total_mass)
-	var combined_vel_2 = (obj_2_vel * obj_2.mass / total_mass) - (obj_1_vel * obj_1.mass / total_mass)
+	# Lighter object does not get a say when it comes to gravity
+	if obj_1.mass > obj_2.mass:
+		obj_2.y_vel = 0
+		obj_2_vel.y = 0
+	elif obj_2.mass > obj_1.mass:
+		obj_1.y_vel = 0
+		obj_1_vel.y = 0
+		
+	# Calculate velocities
+	var combined_vel_1 = obj_1_vel - (obj_2_vel * (obj_2.mass / obj_1.mass))
+	var combined_vel_2 = obj_2_vel - (obj_1_vel * (obj_1.mass / obj_2.mass))
 	print_debug(
 		"comb. vel 1: " + str(combined_vel_1) + "\n" +\
 		"comb. vel 2: " + str(combined_vel_2)
 	)
 	
-	if combined_vel_1.y >= 0:
-		obj_1.y_vel = 0
-		obj_2.y_vel = 0
-	if combined_vel_2.y >= 0:
-		obj_1.y_vel = 0
-		obj_2.y_vel = 0
-	
-	# test movements
+	# test movements one axis at a time
 	var col1 = obj_1.move_and_collide(Vector3(combined_vel_1.x, 0, 0), true)
 	var col2 = obj_2.move_and_collide(Vector3(combined_vel_2.x, 0, 0), true)
 	if col1 or col2:
 		combined_vel_1.x = 0
 		combined_vel_2.x = 0
-		
+	
 	col1 = obj_1.move_and_collide(Vector3(0, combined_vel_1.y, 0), true)
 	col2 = obj_2.move_and_collide(Vector3(0, combined_vel_2.y, 0), true)
 	if col1 or col2:
 		combined_vel_1.y = 0
 		combined_vel_2.y = 0
+		# Y axis is special: Reset acceleration if object did not move on y axis
 		obj_1.y_vel = 0
 		obj_2.y_vel = 0
 		
@@ -44,11 +46,9 @@ func _physics_process(_delta):
 		combined_vel_1.z = 0
 		combined_vel_2.z = 0
 	
+	# Actually move
 	obj_1.move_and_collide(combined_vel_1)
 	obj_2.move_and_collide(combined_vel_2)
-	
-	obj_1_vel = Vector3(0, 0, 0)
-	obj_2_vel = Vector3(0, 0, 0)
 
 func initialize(a: AnimatableBody3D, b: AnimatableBody3D):
 	obj_1 = a
