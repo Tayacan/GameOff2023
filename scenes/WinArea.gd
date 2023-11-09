aa10bf7c3fc318f2e044cd90ac108dc8a3c93a48
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-09 17:23:22 +0100

    Add a nice transition between scenes

--- scenes/WinArea.gd
+++ scenes/WinArea.gd
@@ -4,7 +4,4 @@ extends MeshInstance3D
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("linkable"):
-		timer.start()
-
-func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://scenes/UI/winscreen.tscn")
+		Transition.change_scene("res://scenes/UI/winscreen.tscn")
