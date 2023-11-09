320d8fdb73d53b84efeeba39bec72dadfa922106
Maya Saietz <88489320+ms-tec@users.noreply.github.com>
2023-11-09 11:57:22 +0100

    Remove some blank lines from scripts

--- scenes/UI/ingame_hud.gd
+++ scenes/UI/ingame_hud.gd
@@ -4,7 +4,6 @@ extends Control
 
 @onready var objective_label: Label = $ObjectiveLabel
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	objective_label.text = objective
--- scenes/UI/winscreen_ui.gd
+++ scenes/UI/winscreen_ui.gd
@@ -1,10 +1,8 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-
 
 func _on_menu_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
