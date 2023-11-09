a13aca6d3095d9d37bed5a8fe0e18d24d45cb6f6
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-09 18:58:40 +0100

    Add a second tutorial level

--- scenes/UI/main_menu.gd
+++ scenes/UI/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_start_button_pressed():
-	Transition.change_scene("res://scenes/playground.tscn")
+	Transition.change_scene("res://scenes/puzzles/01_first_link.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
--- scenes/WinArea.gd
+++ scenes/WinArea.gd
@@ -1,5 +1,7 @@
 extends MeshInstance3D
 
+@export_file() var next_level = "res://scenes/UI/winscreen.tscn"
+
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("linkable"):
-		Transition.change_scene("res://scenes/UI/winscreen.tscn")
+		Transition.change_scene(next_level)
