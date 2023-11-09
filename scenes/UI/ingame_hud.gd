f7d280da623620f26b1728350255a2fc881bc970
Maya Saietz <88489320+ms-tec@users.noreply.github.com>
2023-11-09 11:23:15 +0100

    Better win screen

---
+++ scenes/UI/ingame_hud.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var objective: String = ""
+
+@onready var objective_label: Label = $ObjectiveLabel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	objective_label.text = objective
--- scenes/WinArea.gd
+++ scenes/WinArea.gd
@@ -4,4 +4,4 @@ extends MeshInstance3D
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("win-box"):
-		get_tree().change_scene_to_file("res://scenes/winscreen.tscn")
+		get_tree().change_scene_to_file("res://scenes/UI/winscreen.tscn")
