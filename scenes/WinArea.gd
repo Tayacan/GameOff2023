9ed0f069a6cda8a60b649e0c55291c7a9e521e9a
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-09 17:03:22 +0100

    Simple tutorial puzzle

--- scenes/WinArea.gd
+++ scenes/WinArea.gd
@@ -1,5 +1,10 @@
 extends MeshInstance3D
 
+@onready var timer: Timer = $Timer
+
 func _on_area_3d_body_entered(body):
-	if body.is_in_group("win-box"):
-		get_tree().change_scene_to_file("res://scenes/UI/winscreen.tscn")
+	if body.is_in_group("linkable"):
+		timer.start()
+
+func _on_timer_timeout():
+	get_tree().change_scene_to_file("res://scenes/UI/winscreen.tscn")
