50dc7c6a6af6855964e498e70f0e97563c252342
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-15 10:47:21 +0100

    Baisc long-range box manipulation
    Can't lift the box though...

--- player/PlayerController.gd
+++ player/PlayerController.gd
@@ -10,6 +10,7 @@ var control_mode = ControlMode.Walking
 @export var SPEED = 5.0
 @export var PUSH_SPEED = 2.5
 @export var JUMP_VELOCITY = 4.5
+@export var MAX_PUSH_RANGE = 10
 var current_speed = 0
 
 @export var mouse_sensitivity = 1
@@ -34,6 +35,11 @@ func _physics_process(delta):
 func moving_box(delta):
 	if Input.is_action_just_pressed("grab"):
 		control_mode = ControlMode.Walking
+	
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var vel = direction * delta * PUSH_SPEED
+	push(vel)
 
 func walking(delta):
 	# Mode switching
@@ -59,7 +65,7 @@ func walking(delta):
 	else:
 		current_speed = SPEED
 	
-	var current_position = transform.origin
+#	var current_position = transform.origin
 	if is_on_floor():
 		velocity.x = lerp(velocity.x, direction.x * current_speed, delta * 20)
 		velocity.z = lerp(velocity.z, direction.z * current_speed, delta * 20)
@@ -68,7 +74,7 @@ func walking(delta):
 		velocity.z = lerp(velocity.z, direction.z * current_speed, delta * 2)
 	
 	move_and_slide()
-	push(transform.origin - current_position)
+#	push(transform.origin - current_position)
 
 func camera_rotation(mouse_delta: Vector2):
 	head.rotate_y(-mouse_delta.x * mouse_sensitivity * 0.001)
@@ -80,8 +86,8 @@ func camera_rotation(mouse_delta: Vector2):
 	)
 
 func push(vel: Vector3):
-	if is_pushing(vel):
-			obj_to_push.add_velocity(vel)
+	if obj_looking_at and transform.origin.distance_to(obj_looking_at.transform.origin) < MAX_PUSH_RANGE:
+			obj_looking_at.add_velocity(vel)
 
 func is_pushing(dir: Vector3) -> bool:
 	if obj_to_push and obj_to_push.has_method("add_velocity"):
