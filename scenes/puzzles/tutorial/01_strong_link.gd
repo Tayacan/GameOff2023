87a2c667db48f1c7659c199fc8398c857520270b
Maya Saietz <mayasaietz@tayacan.dk>
2023-11-24 15:51:34 +0100

    A bit more tutorial

--- scenes/puzzles/tutorial/01_strong_link.gd
+++ scenes/puzzles/tutorial/01_strong_link.gd
@@ -3,7 +3,8 @@ extends Node3D
 @export_file("*.tscn") var next_level : String
 
 @onready var tutorials = [
-    $Tutorial/Tutorial01
+    $Tutorial/Tutorial01,
+    $Tutorial/Tutorial02
 ]
 
 var current_tutorial = 0
@@ -25,5 +26,13 @@ func _on_pause_menu_game_unpaused():
     tutorials[current_tutorial].show()
 
 
-func _on_win_area_win_area_entered():
+func _on_win_area_2_win_area_entered():
     Transition.change_scene(next_level)
+
+
+func _on_win_area_win_area_entered():
+    set_tutorial(1)
+
+
+func _on_area_3d_body_entered(_body):
+    tutorials[current_tutorial].hide()
